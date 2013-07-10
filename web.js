7f5b8076a2a00420f8748478be61e734a18eee03
Somesh Gupta <someshg@yahoo.com>
2013-07-10 19:13:32 +0000

    updated web.js :w :q e enter the commit message for your changes. Lines starting

--- web.js
+++ web.js
@@ -3,7 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    // Try to read the input file
+    try {
+        var content = fs.readFileSync('index.html', 'utf-8');
+    } catch (error) {
+        console.error('Could not read file "' + file + '". Error: ' + error);
+        process.exit(1);
+    }
+    response.write(content, "binary");
 });
 
 var port = process.env.PORT || 5000;
