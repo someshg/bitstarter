908baa44674695d8b1fa5870fc072cd74efdb593
Somesh Gupta <someshg@yahoo.com>
2013-07-18 21:58:35 +0000

    hw4

--- web.js
+++ web.js
@@ -18,7 +18,7 @@ app.get('/', function(request, response) {
 //    response.send(content, "binary");
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
